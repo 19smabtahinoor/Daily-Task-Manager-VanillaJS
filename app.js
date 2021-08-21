72274b929a3e7015cfb77678947f010dd578b9fd
S.M.ABTAHI NOOR <73340940+19smabtahinoor@users.noreply.github.com>
2021-08-21 19:37:05 +0600

    Done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const addButtton = document.getElementById('add_button');
 const taskList = document.getElementById("task_list");
 let doneTaskCount = 0;
+let deleteCount = 0;
 
 //add the task
 addButtton.addEventListener('click',() => {
@@ -13,30 +14,49 @@ addButtton.addEventListener('click',() => {
             const div = document.createElement('div');
             const p = document.createElement('p');
             const button = document.createElement('button');
+            const button2 = document.createElement('button')
 
             //set the value 
             p.innerText = inputField.value;
             button.innerText = "Done";
+            button2.innerText = "Delete";
 
             //styling the element 
             div.classList.add('single-task', 'flex', 'items-center', 'space-x-4', 'shadow-xl', 'border', 'border-gray-200', 'p-6', 'box-border', 'rounded-md', 'mt-4', 'text-gray-800');
             p.classList.add('text-lg', 'flex', 'flex-grow', 'break-all');
             button.classList.add('done_button', 'bg-blue-600', 'border', 'border-blue-500', 'py-2', 'px-6', 'rounded-md', 'text-white', 'transform', 'focus:scale-105', 'transition', 'duration-300', 'hover:bg-white', 'hover:text-blue-500');
+            button2.classList.add('delete_button', 'bg-red-600', 'border', 'border-red-500', 'py-2', 'px-6', 'rounded-md', 'text-white', 'transform', 'focus:scale-105', 'transition', 'duration-300', 'hover:bg-white', 'hover:text-red-500');
 
             //append the value 
             div.append(p);
             div.append(button);
+            div.append(button2);
 
             taskList.appendChild(div);
             inputField.value = " ";
             doneButtonListener();
+            deleteButtonListener();
             updateResult();
         } else {
-            e.target.placeholder = "Please write something❗❗";
             inputField.classList.add('border-2', 'border-red-500');
         }
     })
 })
+
+//delete task 
+function deleteButtonListener(){
+    const deleteButtons = document.getElementsByClassName('delete_button');
+
+    for (deleteButton of deleteButtons) {
+        deleteButton.addEventListener('click',function(e){
+            e.target.parentNode.remove();
+            deleteCount--;
+            updateResult();
+        })
+    }
+}
+    const task = document.getElementsByClassName("single-task");
+
 
 
 //Completed tasks
@@ -53,6 +73,10 @@ function doneButtonListener(){
                 event.target.innerText = 'Completed';
                 doneTaskCount++;
                 updateResult();
+            }
+        
+            if (typeof (event.target.parentNode) != 'undefined' && event.target.parentNode != null){
+                doneTaskCount = 0;
             }
         })
     }
